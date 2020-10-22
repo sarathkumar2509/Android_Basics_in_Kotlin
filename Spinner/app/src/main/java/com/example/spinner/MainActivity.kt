709e1f7e6d5be7f177f611662c716fd128dd5abd
sarathkumar2509 <smartsarath2509.ss@gmail.com>
2020-10-22 20:29:42 +0530

    Spinner Initial commit

---
+++ Spinner/app/src/main/java/com/example/spinner/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.spinner
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val customList = listOf("One","Two","Three","Four","Five")
+        val adapter = ArrayAdapter(this,R.layout.support_simple_spinner_dropdown_item,customList)
+        sp.adapter=adapter
+
+
+        sp.onItemSelectedListener = object :AdapterView.OnItemSelectedListener{
+            override fun onItemSelected(adapterView: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                Toast.makeText(this@MainActivity,"${adapterView?.getItemAtPosition(position).toString()}",
+                    Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+                TODO("Not yet implemented")
+            }
+
+        }
+
+    }
+}
