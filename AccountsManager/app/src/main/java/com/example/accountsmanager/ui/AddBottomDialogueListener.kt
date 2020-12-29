da6756863611e80edb2f7eda005f90a32d32cb0f
sarathkumar2509 <smartsarath2509.ss@gmail.com>
2020-12-29 17:51:26 +0530

    Initial Commit

--- AccountsManager/app/src/main/java/com/example/accountsmanager/ui/AddBottomDialogueListener.kt
+++
@@ -1,8 +0,0 @@
-package com.example.accountsmanager.ui
-
-import com.example.accountsmanager.ui.db.entity.entity.AccountsItem
-
-interface AddBottomDialogueListener {
-
-    fun onAddButtonClicked(item: AccountsItem)
-}
